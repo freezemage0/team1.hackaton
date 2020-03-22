31faa15232a4e76f6b1c07dd47aff53556c7b537
Nikita Makarenko <makarenko@intervolga.ru>
2020-03-22 18:03:17 +0400

    Кусочек скрипта

--- assets/js/chat/script.js
+++ assets/js/chat/script.js
@@ -1,5 +1,18 @@
 ;(function() {
 
+    let formData = new FormData();
+    formData.append('OFFSET', '0');
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('post', '/chat/getHistory');
+    xhr.send(formData);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            console.log(xhr.responseText);
+        }
+    }
+
+
     $(document).on('click', '.message-send', (e) => {
 
         let message = document.querySelector('.message-input').value;
@@ -24,9 +37,70 @@
     });
 
     class Chat {
+        constructor() {
+        }
+
+        getHistory()  {
+
+        }
     }
 
     class Ajax {
+
+        constructor() {
+            this.xhr = new XMLHttpRequest();
+            this.data = new FormData();
+            this.method = '';
+            this.url = '';
+            this.eventNames = {};
+        }
+
+        setBody(formData) {
+            this.data = formData;
+        }
+
+        bodyAdd(name, value) {
+            this.data.append(name, value)
+        }
+
+        addEventName(eventName, callback) {
+            this.eventNames.eventName = callback;
+        }
+
+        send(longpooling, timeout) {
+
+            if (this.method.length === 0) {
+                console.error('No set method');
+                return false;
+            }
+
+            if (this.url.length === 0) {
+                console.error('No set url');
+                return false;
+            }
+
+            let xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState !== 0) {
+                    xhr.abort();
+                }
+            }
+
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState === 4) {
+                    this.result = xhr.responseText;
+                    return true;
+                }
+            }
+
+            if(longpooling && timeout) {
+                setTimeout(() => {
+                   this.send(longpooling, timeout);
+                }, timeout);
+            }
+
+        }
     }
 
 })();
