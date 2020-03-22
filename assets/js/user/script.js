16fe6b86c15634e5e1a7dfd6335043b09784d27d
Nikita Makarenko <makarenko@intervolga.ru>
2020-03-22 15:14:36 +0400

    Добавил скрипт для валидации формы

--- assets/js/user/script.js
+++ assets/js/user/script.js
@@ -1,7 +1,19 @@
 ;(function() {
 
-    $('.user').on('hidden.bs.collapse', function () {
-        console.log('test');
-    })
+    $('#register').submit(() => {
+        let passConfirm= document.querySelector('#reg-pass-conf');
+
+        let passConfirmValue = passConfirm.value;
+        let passValue = document.querySelector('#reg-pass').value;
+
+        if (passConfirmValue && passConfirmValue.length > 0 && passValue != passConfirmValue) {
+            passConfirm.classList.add('is-invalid');
+            return false;
+        } else {
+            passConfirm.classList.remove('is-invalid');
+        }
+    });
+
+
 
 })();
